ef6a5de0e4ec1e83dce55586d63ce5b2d66b30f3
Alucard <alucardw@duck.com>
2026-01-26 15:17:50 +0000

    Temp Index Update

--- MineBot/index.js
+++ MineBot/index.js
@@ -2,20 +2,64 @@ const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
   host: 'localhost',
-  port: 25565,
+  port: 50948,
   username: 'LocalConTester',
   version: '1.21.6',
   hideErrors: false
 })
 
+const RANGE_GOAL = 3
 
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
+bot.loadPlugin(pathfinder)
 
-  if (message === 'hello') {
-    bot.chat(`Hello ${username}!`)
-  }
+bot.once('spawn', () => {
+  const defaultMove = new Movements(bot)
+
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    if (message === 'come') return
+      const target = bot.players[username]?.entity
+      if (!target) {
+        bot.chat("I don't see you !")
+        return
+      }
+      const { x: playerX, y: playerY, z: playerZ } = target.position
+
+      bot.pathfinder.setMovements(defaultMove)
+      bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
+    if (message === 'hello') {
+      bot.chat(`Hello ${username}!`)
+    }
+
+    if (message === 'attack me')
+      while (bot.players[username].entity.health > 0)
+        attackPlayer(username)
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalNear(playerX, playerY, playerZ, RANGE_GOAL))
+    if (message === 'attack') attackEntity()
+  })
 })
+
+
+function attackPlayer (username) {
+  const player = bot.players[username]
+  if (!player || !player.entity) {
+    bot.chat('I can\'t see you')
+  } else {
+    bot.chat(`Attacking ${player.username}`)
+    bot.attack(player.entity)
+  }
+}
+
+function attackEntity () {
+  const entity = bot.nearestEntity()
+  if (!entity) {
+    bot.chat('No nearby entities')
+  } else {
+    bot.chat(`Attacking ${entity.name ?? entity.username}`)
+    bot.attack(entity)
+  }
+}
 
 bot.on('kicked', reason => {
   console.log('Kicked:', reason)
